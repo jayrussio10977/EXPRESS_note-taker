8e4e96bf09155cd63a2df04fae937a50ec30cd96
jayrussio10977 <120292190+jayrussio10977@users.noreply.github.com>
2023-03-16 21:57:52 -0400

    finalized note taker app

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const fs = require("fs/promises");
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const notes = require("./db/db.json");
 const { response } = require("express");
@@ -7,9 +7,10 @@ const app = express();
 const db = (file) =>
   fs.readFile(`${process.cwd()}/db/${file}`, { encoding: "utf-8" });
 const PORT = process.env.PORT || 3001;
-const util = require('utils.js');
+const util = require("util");
 const { isUtf8 } = require("buffer");
-// const readFromFile = util.promisify(fs.readFile);
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 console.log(notes);
 
@@ -17,70 +18,75 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //   console.log(req, res);
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/api/notes", (req, res) => {
   console.log("getting all notes");
-  res.json(notes)
-})
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.json(notes);
+});
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
-app.get('/db/utils.js', (req, res) =>
-  res.sendFile(path.join(__dirname, '/db/utils.js'))
+app.get("./db/utils.js", (req, res) =>
+  res.sendFile(path.join(__dirname, "/db/utils.js"))
 );
 
-app.post('/api/notes', function(req, res) {
+app.post("/api/notes", function (req, res) {
   const note = req.body;
-  readFileAsync("./db/db.json", "utf-8").then(function (data) {
-   const notes = [].concat(JSON.parse(data));
-   note.id =  notes.length + 1
-   notes.push(note);
-   return notes
-  }).then(function(notes) {
-    writeFileAsync('./db/db.json', JSON.stringify(notes))
-    res.json(notes);
-  })
-})
+  console.log(note);
+  readFileAsync("./db/db.json")
+    .then(function (data) {
+      console.log("promise returned");
+      const notes = [].concat(JSON.parse(data));
+      note.id = notes.length + 1;
+      notes.push(note);
+      console.log(notes);
+      return notes;
+    })
+    .then(function (notes) {
+      writeFileAsync("./db/db.json", JSON.stringify(notes));
+      res.json(notes);
+    });
+});
 
-app.delete('/pi/notes/ :id', function(req, res) {
+app.delete("/api/notes/:id", function (req, res) {
   const idToDelete = parseInt(req.params.id);
-  readFileAsync('./db/db.json', "utf-8").then(function(data) {
-    const notes = [].concat(JSON.parse(data));
-    const newNotesData = []
-    for (let i = 0; i<notes.length; i++) {
-      if(idToDelete !== notes[i].id) {
-        newNotesData.push(notes[i])
+  console.log(idToDelete)
+  readFileAsync("./db/db.json", "utf-8")
+    .then(function (data) {
+      const notes = [].concat(JSON.parse(data));
+      const newNotesData = [];
+      for (let i = 0; i < notes.length; i++) {
+        if (idToDelete !== notes[i].id) {
+          newNotesData.push(notes[i]);
+        console.log(newNotesData)
+        }
       }
-    }
-    return newNotesData
-  }).then(function(notes) {
-    writeFileAsync('./db/db.json', JSON.stringify(notes))
-  })
-})
+      return newNotesData;
+    })
+    .then(function (notes) {
+      writeFileAsync("./db/db.json", JSON.stringify(notes));
+      res.send("saved success!");
+    });
+});
+
+app.get("notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./"));
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 
-
-
-
-
-
-
-
-//   if (req) {
-//   db("db.json")
-//     .then((value) => {
-//       // console.log(JSON.parse (value));
-//       if (!value) res.status(400).send("no notes found");
-//       res.status(200).json({ message: "this gets all the notes", value });
-//     })
-//     .catch((err) => res.status(500).send("internal server err"));
-//   //   }
-// });
 
 app.listen(PORT, () => console.log("express app listening on port 3001"));
